70db4cf5a57bc96dd98a1a1ed6c99d5c99857bdd
Almidi <vailakis7@yahoo.gr>
2019-05-09 20:17:47 +0300

    configured file service image

---
+++ portreto/setup-rs/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ************************************************
+echo Starting the replica set
+echo ************************************************
+
+sleep 10 | echo Sleeping
+mongo mongodb://mongo-rs0-1:27017 replicaSet.js
---
+++ portreto/storage/django/init.sh
@@ -0,0 +1,4 @@
+# link data
+ln -s /data /app/data
+# Run server
+python manage.py runserver 0.0.0.0:8000
